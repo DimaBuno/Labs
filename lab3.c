d9866966f465b328ef4799fce7af67e2cd63adbe
DimaBuno <32478252+DimaBuno@users.noreply.github.com>
2017-10-25 10:39:40 +0300

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,17 +1,20 @@
 #include "stdio.h"
 int main(void) {
-char str[5];
+char str[200];
 char sym = str[0];
 int m = 0;
 int k=0;
 scanf("%s",&str);
-for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
+for (int i = 0; i < strlen(str) ; i++) {
+    for (int j = 0; j < strlen(str) ; j++) {
       if (j == i) j++;
       if (str[i] == str[j]) k++;
     }
     if (k>m){ 
     k=m;sym=str[i];
+    }
+    if (k=1){
+    sym=str[i];
     }
 }
 printf("%c",sym);
